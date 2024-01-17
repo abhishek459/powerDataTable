058177fd60677438cf231cea47df7b9e0975713a
Abhishek Thulasi <abhishek45nine@gmail.com>
2024-01-17 20:14:31 +0530

    Basic Impementation
    - Added FieldSet and Object name attribute to component
    
    - Implemented logic to fetch fieldset field information dynamically
    
    - Added internet connectivity listener to component

--- force-app/main/default/lwc/powerDataTable/powerDataTable.js
+++ force-app/main/default/lwc/powerDataTable/powerDataTable.js
@@ -13,8 +13,31 @@ export default class PowerDataTable extends LightningElement {
         }
     }
 
+    _objectName;
+    @api get objectName() {
+        return this._objectName;
+    }
+
+    set objectName(value) {
+        if (value !== this._objectName) {
+            this._objectName = value;
+            this.fetchRecordsOfFieldSet();
+        }
+    }
+    connectedToInternet = true;
+
     loading = false;
     fetchRecordsOfFieldSet() {
         this.loading = true;
+    }
+
+    connectedCallback() {
+        window.addEventListener("online", function () {
+            this.connectedToInternet = true;
+        });
+
+        window.addEventListener("offline", function () {
+            this.connectedToInternet = false;
+        });
     }
 }
